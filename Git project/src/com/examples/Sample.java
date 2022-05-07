39173224bf969ff4780962b05b52ae59602f8e58
ravina <t.ravinacbe@gmail.com>
2022-05-07 15:10:40 +0530

    Sample first level project

---
+++ Git project/src/com/examples/Sample.java
@@ -0,0 +1,22 @@
+package com.examples;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		
+		int id = 10;
+		
+		switch(id) {
+		case 10:
+			System.out.println("Value is 10");
+			break;
+		case 20:
+			System.out.println("Value is 20");
+			break;
+			
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
